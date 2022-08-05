0a3ffd2d844c445338b48cfc6f1f94c159fafa45
Abdel_2020 <abdelnoura97@gmail.com>
2022-08-05 20:24:27 +0100

    Can now read input from search!!

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-
 /*After User hits enter or clicks the button,
 store the value in the search in a variable.*/
 
 const readInput = () => {
     const ipSearchValue = document.getElementById("ip-search");
-    const ipSearchBtn = document.getElementsByClassName(".ip-search-btn") ;
- 
-
-
-ipSearchBtn.addEvenListener('click', (e) => {
-    e.preventDefault();
-    console.log(`${ipSearchValue.value}`);
-
-});
+    const ipSearchBtn = document.getElementsByClassName("ip-search-btn")[0];
 
 
 
+    ipSearchBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(`${ipSearchValue.value}`);
+        return ipSearchValue.value;
+    });
 
 
-} 
 
+
+
+}
+
+
+readInput();
