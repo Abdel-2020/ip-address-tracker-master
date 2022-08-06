4faf53cc67f551228b84a1b9cf82118681835b79
Abdel_2020 <abdelnoura97@gmail.com>
2022-08-06 20:07:31 +0100

    Reformatted markup

--- index.js
+++ index.js
@@ -6,18 +6,21 @@ const readInput = () => {
     const ipSearchBtn = document.getElementsByClassName("ip-search-btn")[0];
 
 
-
     ipSearchBtn.addEventListener('click', (e) => {
         e.preventDefault();
         console.log(`${ipSearchValue.value}`);
-        return ipSearchValue.value;
     });
-
-
-
-
 
 }
 
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
 
 readInput();
+fetchMaps();
