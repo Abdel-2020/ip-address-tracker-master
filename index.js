5c4207a8aee5c6792f49cf95e10dc2f9b3a7fd9d
Abdel_2020 <abdelnoura97@gmail.com>
2022-08-07 18:05:52 +0100

    cleaned up JS

--- index.js
+++ index.js
@@ -43,12 +43,15 @@ const findIp = (ip) => {
     fetch(`http://ip-api.com/json/${ip}?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp,org,as,query`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.isp);
+            console.log(data);
+            let ipData = [data.query, data.city, "N/A", data.isp]
             let ipDetails = document.querySelectorAll(".ip-details-text"); 
-            ipDetails[0].innerHTML = data.query;
-            ipDetails[1].innerHTML = data.city;
-            ipDetails[2].innerHTML = "N/A";
-            ipDetails[3].innerHTML = data.isp;
+            
+            for (let i= 0; i < ipDetails.length; i++){
+
+                ipDetails[i].innerHTML = ipData[i];
+
+            }
 
         })
 
@@ -65,12 +68,14 @@ const findIp = (ip) => {
 
 
 
-let map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([51.505, -0.09],  19);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
+
 
 let myIcon = L.icon({
     iconUrl: 'images/icon-location.svg',
